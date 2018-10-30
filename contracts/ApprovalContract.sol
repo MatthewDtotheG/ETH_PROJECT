4ce348c26bb77a6f59f4a5871bfd8429ee25f5ba
MatthewDtotheG <mdgarrett1984@gmail.com>
2018-10-30 17:30:59 -0400

    Adds contract tests

--- contracts/ApprovalContract.sol
+++ contracts/ApprovalContract.sol
@@ -4,7 +4,7 @@ contract ApprovalContract {
 
   address public sender;
   address public receiver;
-  address public approver;
+  address constant public approver = 0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef;
 
   function deposit(address _receiver) external payable {
     require(msg.value > 0);
